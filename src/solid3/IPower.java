fbdee7e15ab0e9fb78633ffe8ad30435172e8924
Harshinikr <hkulanda@asu.edu>
2025-04-29 17:26:26 -0700

    Update IPower.java

--- src/solid3/IPower.java
+++ src/solid3/IPower.java
@@ -4,50 +4,49 @@ import java.util.Map;
 import java.util.HashMap;
 
 public interface IPower {
-	int toPower(int n, int pow);
+    int toPower(int n, int pow);
 }
 
 class PowerSimple implements IPower {
-	public int toPower(int n, int pow) {
-		if (pow == 0)
-			return 1;
-		int res = 1;
-		for (int i = 0; i < pow; res *= n, i++)
-			;
-		;
-		;
-		return res;
-	}
+    @Override
+    public int toPower(int n, int pow) {
+        if (pow == 0) {
+            return 1;
+        }
+        int res = 1;
+        for (int i = 0; i < pow; i++) {
+            res *= n;
+        }
+        return res;
+    }
 }
 
 class PowerCached implements IPower {
-	private static Map<Integer, Map<Integer, Integer>> __cache = new HashMap<Integer, Map<Integer, Integer>>();
+    private static Map<Integer, Map<Integer, Integer>> cache = new HashMap<>();
 
-	// resolves the inefficiency in AlmostBest
-	public int toPower(int n, int pow) {
-		Map<Integer, Integer> entry = PowerCached.__cache.get(n);
+    @Override
+    public int toPower(int n, int pow) {
+        Map<Integer, Integer> entry = cache.get(n);
 
-		if (entry == null) {
-			entry = new HashMap<Integer, Integer>();
-		}
-		return toCachedPower(entry, n, pow);
-	}
+        if (entry == null) {
+            entry = new HashMap<>();
+            cache.put(n, entry);
+        }
+        return toCachedPower(entry, n, pow);
+    }
 
-	// The contract here is we know there is a cache entry so no check required
-	// (DBC)
-	private int toCachedPower(Map<Integer, Integer> e, int n, int pow) {
-		Integer res = null;
-		if (pow == 0) {
-			res = 1;
-		} else {
-			res = e.get(pow);
-			if (res != null) {
-				return res;
-			} else {
-				res = n * toCachedPower(e, n, pow - 1);
-				e.put(pow, res); // saves each intermediate result in the cache
-			}
-		}
-		return res;
-	}
+    private int toCachedPower(Map<Integer, Integer> e, int n, int pow) {
+        if (pow == 0) {
+            return 1;
+        }
+
+        Integer res = e.get(pow);
+        if (res != null) {
+            return res;
+        } else {
+            res = n * toCachedPower(e, n, pow - 1);
+            e.put(pow, res);
+            return res;
+        }
+    }
 }
